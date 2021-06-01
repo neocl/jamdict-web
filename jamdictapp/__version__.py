b150820e6d1c229f35a2a9444f804260e94b8757
Le Tuan Anh <tuananh.ke@gmail.com>
2021-06-01 21:28:54 +0800

    add version info

---
+++ jamdictapp/__version__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Jamdict-web - Japanese Reading Assistant
+
+# This code is a part of jamdict-web library: https://github.com/neocl/jamdict-web
+# :copyright: (c) 2021 Le Tuan Anh <tuananh.ke@gmail.com>
+# :license: MIT, see LICENSE for more details.
+
+__author__ = "Le Tuan Anh"
+__email__ = "tuananh.ke@gmail.com"
+__copyright__ = "Copyright (c) 2021, Le Tuan Anh"
+__credits__ = []
+__license__ = "MIT License"
+__description__ = "Free and open-source Japanese Reading Assistant with morphological analyser, Japanese-English dictionary, Kanji dictionary, and Japanese Names dictionary"
+__url__ = "https://github.com/neocl/jamdict-web"
+__maintainer__ = "Le Tuan Anh"
+__version_major__ = "1.0"
+__version__ = "{}b1".format(__version_major__)
+__status__ = "4 - Beta"
+__version_long__ = "{} - {}".format(__version_major__, __status__)
